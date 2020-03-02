023a03d0847a86e1fb6694ed75c8f08e273815f5
Riccardo Tartaglia <riccardo.tartaglia@webeetle.it>
2020-03-02 14:23:15 +0100

    prefix added

--- cerbero.js
+++ cerbero.js
@@ -47,7 +47,34 @@ module.exports = fp(async function (fastify, opts) {
       const {headers} = request
       const rules = fastify.cerbero.rules;
       const [host, port = 80] = headers.host.split(':')
-      return rules.find(rule => rule.listener.host === host)
+      const rule =  rules.find(rule => rule.listener.host === host)
+      if (!rule) throw new Error()
+
+      // check if the url match
+      let pureUrl = false
+      let checkPath = false
+      let route = false;
+      for(let key in rule.route) {
+        route = rule.route[key]
+        const regex = route.regex ? route.regex : '/(.*)'
+        const prefix = route.prefix ? route.prefix : ''
+        const regexp = pathToRegexp(regex)
+        if(prefix.length > 0){
+          pureUrl = request.req.url.replace(prefix, '')
+          if(pureUrl.length === request.req.url.length) continue
+        } else {
+          pureUrl = request.req.url
+        }
+        checkPath = regexp.exec(pureUrl);
+        if (checkPath) {
+          break
+        }
+      }
+      if (!route) throw new Error()
+      if (!checkPath) throw new Error()
+
+      return [route, pureUrl];
+
     }
   }
 
@@ -56,22 +83,14 @@ module.exports = fp(async function (fastify, opts) {
   fastify.setNotFoundHandler((request, reply) => {
     try {
       // check if found a rule for this host
-      const rule = fastify.cerbero.match(request)
-      if (!rule) throw new Error()
-
-      // check if the url match
-      const regex = rule.route.regex ? rule.route.regex : '/(.*)'
-      const regexp = pathToRegexp(regex)
-      const checkPath = regexp.exec(request.req.url);
-      if (!checkPath) throw new Error()
+      const [route, pureUrl] = fastify.cerbero.match(request)
 
       const {proxy} = require('fast-proxy')({
-        base: rule.route.target
+        base: route.target
       })
 
-      proxy(request.req, reply.res, request.req.url, {})
+      proxy(request.req, reply.res, pureUrl, {})
     } catch (e) {
-      console.log(e.message)
       reply.type('text/html')
       reply.send(`<h1 style="text-align: center">Resource Not Found</h1><p style="text-align: center">Cerbero v0.0.1</p>`, 404)
     }
